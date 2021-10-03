dc3f48317bc335f6ebc3f887776648ba7ba9112a
Naveen Pattanashetti <pattanashettinaveen.com>
2021-10-03 15:20:41 +0530

    Emp_Part_Time

--- src/com/bridgelabz/Employeewage/Employee_wage_program.java
+++ src/com/bridgelabz/Employeewage/Employee_wage_program.java
@@ -2,7 +2,7 @@ package com.bridgelabz.Employeewage;
 
 public class Employee_wage_program {
 	public static void main(String[] args) {
-		Employee_wage_program employeewage=new Employee_wage_program();
+		Employee_wage_program employeewage = new Employee_wage_program();
 		employeewage.welcome();
 		employeewage.emopDailyWage();
 	}
@@ -12,17 +12,19 @@ public class Employee_wage_program {
 	}
 
 	public void emopDailyWage() {
-		int Is_Full_Time = 1;
+		int Is_Part_Time = 1;
+		int Is_Full_Time = 2;
 		int EMP_RATE_PER_HOUR = 20;
 		int empHrs = 0;
 		int empWage = 0;
-		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-		if (empCheck == Is_Full_Time)
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		if (empCheck == Is_Part_Time)
+			empHrs = 4;
+		else if (empCheck == Is_Full_Time)
 			empHrs = 8;
 		else
 			empHrs = 0;
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Employee_wage:" + empWage);
 	}
-
 }
