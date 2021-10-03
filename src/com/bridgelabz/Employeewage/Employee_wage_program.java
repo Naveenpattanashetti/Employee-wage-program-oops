73e686578c8e9a12543f8ecbfcfd7ac8ab0e8bbd
Naveen Pattanashetti <pattanashettinaveen.com>
2021-10-03 14:41:25 +0530

    added:Employee_Is_Present_Or_Not

--- src/com/bridgelabz/Employeewage/Employee_wage_program.java
+++ src/com/bridgelabz/Employeewage/Employee_wage_program.java
@@ -1,11 +1,21 @@
 package com.bridgelabz.Employeewage;
 
-public class Employee_wage_program {
-
+public class Employee_wage_program  {
 	public static void main(String[] args) {
-		System.out.println("welcome to employee wage computation program");
-		
-
+		welcome();
+		attendanceCheck();
 	}
 
+	static void welcome() {
+		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+	}
+
+	static void attendanceCheck() {
+		int isFullTime = 1;
+		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+		if (empCheck == isFullTime)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
+	}
 }
